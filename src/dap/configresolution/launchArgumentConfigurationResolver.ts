ec9391d4399b80061678556ab0353a5f09c6b5c5
joakimlo <joakim.lonnberg@iar.com>
2021-06-23 06:14:46 +0200

    Resolve to use common utils

--- src/dap/configresolution/launchArgumentConfigurationResolver.ts
+++ src/dap/configresolution/launchArgumentConfigurationResolver.ts
@@ -2,7 +2,8 @@
 
 import { CSpyLaunchRequestArguments } from "../cspyDebug";
 import { BaseConfigurationResolver, PartialSessionConfiguration } from "./baseConfigurationResolver";
-import { CspyOsUtils} from "../cspyUtils";
+import { IarOsUtils } from "../../utils/osUtils";
+import { IarUtils } from "../../utils/iarUtils";
 import * as Path from "path";
 import * as Fs from "fs";
 
@@ -15,9 +16,17 @@ export class LaunchArgumentConfigurationResolver extends BaseConfigurationResolv
 
     resolveLaunchArgumentsPartial(args: CSpyLaunchRequestArguments): Promise<PartialSessionConfiguration> {
 
+		var targets = IarUtils.getTargetsFromEwPath(args.workbenchPath);
+		var target:string = "";
+		if(targets.length > 0){
+			// Just use the first one in the list for now.
+			target = targets[0];
+			console.log("Using target " + target);
+		}
+
+
 		// Resolve the target from the the provided path.
 		var folders = Fs.readdirSync(args.workbenchPath);
-		var target:string = "";
 		for(var i = 0; i < folders.length; i++){
 			var possibleTarget = Path.basename(folders[i]);
 			if(possibleTarget !== "common"){
@@ -32,16 +41,16 @@ export class LaunchArgumentConfigurationResolver extends BaseConfigurationResolv
 
 		const plugins = args.plugins? args.plugins : [];
 		// always add bat library.
-		plugins.push(CspyOsUtils.resolveTargetLibrary(args.workbenchPath,target,"Bat"));
+		plugins.push(IarOsUtils.resolveTargetLibrary(args.workbenchPath,target,"Bat"));
 
 		const macros = args.macros? args.macros : [];
 
         const config: PartialSessionConfiguration = {
             attachToTarget: false,
-            driverName: CspyOsUtils.resolveTargetLibrary(args.workbenchPath,target,args.driverLib),
-            processorName: CspyOsUtils.resolveTargetLibrary(args.workbenchPath,target,"PROC"),
+            driverName: IarOsUtils.resolveTargetLibrary(args.workbenchPath,target,args.driverLib),
+            processorName: IarOsUtils.resolveTargetLibrary(args.workbenchPath,target,"PROC"),
             type: "simulator",
-            options: ["--plugin=" + CspyOsUtils.resolveTargetLibrary(args.workbenchPath,target,"Bat"),"--backend"].concat(args.driverOptions? args.driverOptions : []),
+            options: ["--plugin=" + IarOsUtils.resolveTargetLibrary(args.workbenchPath,target,"Bat"),"--backend"].concat(args.driverOptions? args.driverOptions : []),
             plugins: plugins,
             setupMacros: macros,
             target: target
