77681164f946cef1c88be59cdf1d8cdf5a9ccf45
Hampus Adolfsson <hampus.adolfsson@iar.com>
2022-02-10 15:38:10 +0100

    VSC-176 Add generated svd to launch configuration

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,19 @@
 
 
 import * as vscode from "vscode";
-import { CSpyDebugSession } from "./dap/cspyDebug";
+import { CSpyDebugSession, CSpyLaunchRequestArguments } from "./dap/cspyDebug";
 import { DebugSessionTracker } from "./debugSessionTracker";
 import { BreakpointCommands } from "./breakpointCommands";
 import { CSpyConfigurationResolver } from "./configresolution/cspyConfigurationResolver";
 import { CSpyConfigurationsProvider } from "./configresolution/cspyConfigurationsProvider";
 import { SettingsConstants } from "./settingsConstants";
 import { BreakpointType } from "./dap/breakpoints/cspyBreakpointManager";
+import { tmpdir } from "os";
+import * as crypto from "crypto";
+import * as path from "path";
+import * as fs from "fs";
+import * as fsProm from "fs/promises";
+import { SvdGenerator } from "./svdGenerator";
 
 let sessionTracker: DebugSessionTracker | undefined;
 
@@ -34,6 +40,20 @@ export function activate(context: vscode.ExtensionContext) {
             const actualType = bpType === SettingsConstants.BreakpointTypeValues.HARDWARE ? BreakpointType.HARDWARE :
                 bpType === SettingsConstants.BreakpointTypeValues.SOFTWARE ? BreakpointType.SOFTWARE : BreakpointType.AUTO;
             config["breakpointType"] = actualType;
+        }
+    });
+
+    // Generate and locate an svd for the session, so that the register view is populated
+    CSpyConfigurationResolver.getInstance().addModifier(async(config) => {
+        if (!config["svdFile"]) {
+            // Create a file name based on the configuration. If it exists, we've already generated an svd for this config.
+            const outPath = path.join(tmpdir(), "iar-vsc-svd", crypto.createHash("md5").update(JSON.stringify(config)).digest("hex") + ".svd");
+            if (!fs.existsSync(outPath)) {
+                await fsProm.mkdir(path.dirname(outPath), {recursive: true});
+                const svdData = await SvdGenerator.generateSvd(config as unknown as CSpyLaunchRequestArguments);
+                await fsProm.writeFile(outPath, SvdGenerator.toSvdXml(svdData));
+            }
+            config["svdFile"] = outPath;
         }
     });
 }
