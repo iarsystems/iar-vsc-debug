5815e801442d69bb3b5ab8bbf33a53f8fb7d8494
Hampus Adolfsson <hampus.adolfsson@iar.com>
2026-01-27 17:21:57 +0100

    VSC-554 Fix some unnecessary listwindow redraws

--- src/listwindows/listwindowBackend.ts
+++ src/listwindows/listwindowBackend.ts
@@ -62,6 +62,9 @@ export class ListWindowBackendHandler<T extends ListWindowBackend.Client> {
             // immediately, since it affects how they respond to notifications
             // in the background.
             if (msg.subject === "viewportChanged") {
+                if (this.numberOfVisibleRows === msg.rowsInPage) {
+                    return;
+                }
                 this.numberOfVisibleRows = msg.rowsInPage;
                 for (const controller of this.sessions.values()) {
                     controller.handleMessageFromView(msg);
